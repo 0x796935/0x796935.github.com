57869a95c863b30985f4d733995ae9b9801183a8
0x796935 <65812282+0x796935@users.noreply.github.com>
2023-09-02 18:43:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 async function checkFileExtension() {
+  const fileStatus = document.getElementById('fileStatus');
+  fileStatus.innerHTML = 'Checking file extension...<br>';
+
   const fileInput = document.getElementById('fileInput');
   const file = fileInput.files[0];
   const fileName = file.name;
   const fileExtension = fileName.split('.').pop();
+  const folderName = fileName.split('.').slice(0, -1).join('.');
+
 
   if (fileExtension === 'zip') {
-    const zip = new JSZip();
-    const folderName = fileName.split('.').slice(0, -1).join('.');
-
+    var zip = new JSZip();
+    
     try {
+      fileStatus.innerHTML += 'Unzipping file...<br>';
       await zip.loadAsync(file);
       // console.log(zip)
       
@@ -17,13 +22,15 @@ async function checkFileExtension() {
 
       if(!bLevelDatExists){
         console.log("The .zip file does not contain a world save")
+        fileStatus.innerHTML += 'Error: The .zip file does not contain a world save<br>';
         return "The .zip file does not contain a world save";
       }
       // list all level.dat files in the zip file
       var levelDatFiles = zip.file(/level\.dat\d+$/);
-      console.log(levelDatFiles)
+      // console.log(levelDatFiles)
+      // console.log(`Found ${levelDatFiles.length} level.dat files in the .zip file`);
+      fileStatus.innerHTML += `Found ${levelDatFiles.length} level.dat files in the .zip file<br>`;
 
-      console.log(`Found ${levelDatFiles.length} level.dat files in the .zip file`);
 
       if(levelDatFiles.length < 1){
         console.log("The .zip doesn't contain a level.datX file")
@@ -41,6 +48,8 @@ async function checkFileExtension() {
         var inflatedLevelDat 
         try{
           console.log(`Inflating ${levelDatFile.name}`)
+          fileStatus.innerHTML += `Inflating ${levelDatFile.name}<br>`;
+
           inflatedLevelDat = pako.inflate(content);
           // console.log(inflatedLevelDat)
           // convert inflatedLevelDat to hex string without Buffer becuase this is a browser
@@ -60,6 +69,7 @@ async function checkFileExtension() {
           while(hex.indexOf('ffff000100') !== -1) {
             hex = hex.replace(/ffff000100/g, 'ffff000000')
             console.log(`[+] Removed cheat flag from ${levelDatFile.name}`)
+            fileStatus.innerHTML += `Removed cheat flag from ${levelDatFile.name}<br>`;
           }
           
           console.log(hex.indexOf('ffff000100'));
@@ -75,30 +85,29 @@ async function checkFileExtension() {
           zip.file(`${levelDatFile.name}`, deflatedLevelDat);
           console.log(zip.file(levelDatFile.name))
 
-          const zipFile = await zip.generateAsync({type:"blob"});
-
-          // create new blob and log url
-          const blob = new Blob([zipFile], {type: "application/zip"});
-          const url = URL.createObjectURL(blob);
-
-          console.log(url);
+          zip = await zip.generateAsync({type:"blob"});
 
           // create download link and click it
 
 
         }catch(e) {
           console.log(e)
+          fileStatus.innerHTML += `Error inflating ${levelDatFile.name}<br>`;
           return "Error inflating level.dat file";
         }
 
         levelDatFilesByteArray.push({name: levelDatFile.name, content: content, inflated: inflatedLevelDat});
       }
 
-      console.log(`Found ${levelDatFilesByteArray.length} level.dat files in the .zip file`);
+      // console
+      console.log(`Found and changed ${levelDatFilesByteArray.length} level.dat files in the .zip file`);
       console.log(`The first level.dat file is:`)
       console.log(levelDatFilesByteArray[0].inflated);
+      // html
+      fileStatus.innerHTML += `Found and changed ${levelDatFilesByteArray.length} level.dat files in the .zip file<br>`;
 
     } catch (error) {
+      fileStatus += 'Error reading the .zip file<br>';
       console.error('Error reading the .zip file:');
       console.log(error);
       return "Error reading the .zip file";
@@ -107,4 +116,19 @@ async function checkFileExtension() {
     console.log('The file is not a .zip file');
     return "The file is not a .zip file";
   }
+
+  // create new blob and log url
+  const blob = new Blob([zip], {type: "application/zip"});
+  const url = URL.createObjectURL(blob);
+
+  // create download link and click it
+  var downloadLink = document.createElement('a');
+  downloadLink.href = url;
+  downloadLink.download = `${folderName}.zip`;
+
+  fileStatus.innerHTML += `<a id="download" href="${url}">Download new Savegame</a><br>`;
+
+  document.body.appendChild(downloadLink);
+  console.log(`Download: ${url}`);
+
 }
